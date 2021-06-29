578bbc9cb86877992a221aa4f8d4b5310b53a571
ajomuch92 <aaronmontes_92@yahoo.es>
2021-06-29 14:13:16 -0600

    Formatting files and updating package version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,4 @@
 <!-- Date Formatt (DD/MM/YYY) -->
 ## 0.1.0 First release (29/06/2021)
+
+## 0.1.1 First release (29/06/2021)
--- lib/simple_connection_checker.dart
+++ lib/simple_connection_checker.dart
@@ -3,19 +3,17 @@ library simple_connection_checker;
 import 'dart:io';
 
 class SimpleConnectionChecker {
-
   /// Static method to check if it's connected to internet
   /// lookUpAddress: String to use as lookup address to check internet connection
   static Future<bool> isConnectedToInternet({String? lookUpAddress}) async {
     try {
-      if(lookUpAddress == null) {
+      if (lookUpAddress == null) {
         lookUpAddress = 'www.google.com';
       }
       final result = await InternetAddress.lookup(lookUpAddress);
-      if(result.isNotEmpty && result[0].rawAddress.isNotEmpty)
-        return true;
+      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) return true;
       return false;
-    } on SocketException catch(_) {
+    } on SocketException catch (_) {
       return false;
     }
   }
