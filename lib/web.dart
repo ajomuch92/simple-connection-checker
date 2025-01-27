902e37f4e613c2c33603dc32a5542653a86707da
ajomuch92 <aaronmontes_92@yahoo.es>
2025-01-27 13:37:22 -0600

    fix: 🐛 Fixing bugs with url format and timeout

--- lib/web.dart
+++ lib/web.dart
@@ -1,11 +1,17 @@
 import 'package:http/http.dart' as http;
 
-/// Private method to verify when there is internet connection for web, using the http package
+/// Checks if there is an internet connection on the web using the `http` package.
+/// [url]: The URL to which the connection will be attempted (e.g., 'www.google.com').
+/// Returns `true` if the connection is successful, otherwise `false`.
 Future<bool> checkInternetConnectionWeb(String url) async {
+  // Ensure the URL is properly formatted
+  if (!url.startsWith(RegExp(r'https?://'))) {
+    url = 'https://$url'; // Add 'https://' if not present
+  }
   var client = http.Client();
   try {
     var uri = Uri.https(url);
-    var response = await client.get(uri);
+    var response = await client.get(uri).timeout(const Duration(seconds: 5));
     // If you get here, the connection was successful
     return response.statusCode == 200;
   } catch (e) {
