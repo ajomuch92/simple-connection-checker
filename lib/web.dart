e4d8b1f23a600ff301f3c5728745239f648d0575
ajomuch92 <aaronmontes_92@yahoo.es>
2025-01-27 11:12:04 -0600

    refactor: 💡 Updating method to check connection for web

--- lib/web.dart
+++ lib/web.dart
@@ -1,13 +1,17 @@
-import 'dart:js' as js;
+import 'package:http/http.dart' as http;
 
-/// Private method to verify when there is internet connection for web, using the fetch method from JS
+/// Private method to verify when there is internet connection for web, using the http package
 Future<bool> checkInternetConnectionWeb(String url) async {
+  var client = http.Client();
   try {
-    await js.context.callMethod('fetch', [url]);
+    var uri = Uri.https(url);
+    var response = await client.get(uri);
     // If you get here, the connection was successful
-    return true;
+    return response.statusCode == 200;
   } catch (e) {
     // If an error occurs, there is no connection
     return false;
+  } finally {
+    client.close();
   }
 }
